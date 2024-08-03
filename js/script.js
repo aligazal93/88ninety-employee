dd640c0183feb439a50fa0b9f2289f8facc337cb
Ali <aligazal.wd93@gmail.com>
2024-08-03 16:30:45 +0300

    - initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+function myFunction2() {
+    var x = document.getElementById("myInputReset");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
